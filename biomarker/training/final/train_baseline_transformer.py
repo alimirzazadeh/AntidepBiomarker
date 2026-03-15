392dfad683e25d494de595e0bbaf50b30874ff9b
alimirzazadeh <alimirzazadeham@gmail.com>
2026-03-15 11:12:44 -0400

    .

--- biomarker/training/final/train_baseline_transformer.py
+++ biomarker/training/final/train_baseline_transformer.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 import itertools
 import os
 import torch
@@ -401,7 +402,7 @@ for fold in range(0,1):
     print("Total Trainable Parameters: {:,}".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
 
     pretrained = '' 
-    exp_name = f"BASELINE_{args.label}_{dataset_name}_lr_{lr}_bs_{batch_size}_steps_{num_steps}_dpt_{args.dropout}_fold{fold}{pretrained}_heads{args.num_heads}_{add_name}_featuredim_{args.feature_dim}_numtokenheads_{args.num_token_heads}_{'trn_resmp' if args.training_resample else ''}_{'NOISE' if args.NOISE_PADDING else ''}_{'TAIL'+str(args.tail_length_vit) if args.tail_length_vit >=0 else ''}_wd_{str(round(args.weight_decay,4))}_{'focal'+str(round(args.gamma,2)) + str(round(args.alpha)) if args.focal_loss else 'bce'}"
+    exp_name = f"BASELINE_fold{fold}_{datetime.now().strftime('%Y%m%d')}" #{args.label}_{dataset_name}_lr_{lr}_bs_{batch_size}_steps_{num_steps}_dpt_{args.dropout}_fold{fold}{pretrained}_heads{args.num_heads}_{add_name}_featuredim_{n_model.model.args.feature_dim}_numtokenheads_{args.num_token_heads}_{'trn_resmp' if args.training_resample else ''}_{'NOISE' if args.NOISE_PADDING else ''}_{'TAIL'+str(args.tail_length_vit) if args.tail_length_vit >=0 else ''}_wd_{str(round(args.weight_decay,4))}_{'focal'+str(round(args.gamma,2)) + str(round(args.alpha)) if args.focal_loss else 'bce'}"
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
